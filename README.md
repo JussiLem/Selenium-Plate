083d4f36785c47014280ed1bf0b57d562afd62c2
Jussi Lemmetyinen <jussi.lem@gmail.com>
2018-08-18 15:41:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## INFO ##
+
+Boilerplate type of project for Selenium, with gradle build and flogger logging. 
+
+#Note: 
+You have to download the webdrivers yourself.
---
+++ src/test/java/jussilemmetyinen/me/utils/EnvironmentManager.java
@@ -0,0 +1,32 @@
+package jussilemmetyinen.me.utils;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class EnvironmentManager {
+    protected static WebDriver driver;
+
+    @BeforeAll
+    public static void setUp() {
+        // Tell the app where the driver is
+        System.setProperty("webdriver.chrome.driver", "webdrivers\\chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        // Drive Chrome in headless mode
+        chromeOptions.addArguments("--headless");
+        // Open Chrome
+        driver = new ChromeDriver(chromeOptions);
+
+        // Remove all cookies
+        driver.manage().deleteAllCookies();
+
+        // Wait until close
+        //      Thread.sleep(7000);
+
+        // Suljetaan selain ja ajuri
+        //driver.close();
+
+    }
+}
+
